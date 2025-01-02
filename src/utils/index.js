6a4ae5587d8a55131b97ad8f198c4d0a543439f5
lawrenceku <lawrencekunmilola@gmail.com>
2025-01-02 23:10:59 +0100

    Fix: name casing change for the quiz component

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-import Quiz from "../pages/quiz";
+import Quiz from "../pages/Quiz";
 import QuizContainer from "../pages/QuizContainer";
 import SignIn from "../pages/auth/Signin";
 import SignUp from "../pages/auth/SignUp";
