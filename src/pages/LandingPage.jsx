76bb5d0fb5d9408573517d34eeb6a4f48e9699d2
Lawrence <lawrencekunmilola@gmail.com>
2025-01-09 14:11:36 +0100

    Add persistence to app

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,6 +5,10 @@ import Navbar from "../component/landing-page/NavBar";
 import TeamSection from "../component/landing-page/Team";
 
 const LandingPage = () => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+        window.location.href = '/quiz';
+    }
     return (
         <div>
         <Navbar />
